f5e3d24c9edcf66b1fc4c11888b84e36cef00994
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-01-31 17:55:12 +0100

    refactor: Possibilité de reprendre la génération

--- generate.php
+++ generate.php
@@ -7,46 +7,55 @@ $letters = range('a', 'z');
 $specialChars = [' ', ',', '.'];
 $alphabet = array_merge($letters, $specialChars);
 
+$req = $bdd->prepare('SELECT id, location, wall, shelf, volume FROM books ORDER BY id DESC LIMIT 1');
+$req->execute([]);
+$lastBook = $req->fetch(PDO::FETCH_ASSOC);
+
+$hexagonNumber = '0';
+$wall = 1;
+$shelf = 1;
+$volume = 1;
+$lastPage = 0;
+
+if ($lastBook) {
+    $hexagonNumber = $lastBook['location'];
+    $wall = $lastBook['wall'];
+    $shelf = $lastBook['shelf'];
+    $volume = $lastBook['volume'];
+
+    $req = $bdd->prepare('SELECT MAX(page) AS lastPage FROM pages WHERE book_id = :book_id');
+    $req->execute(['book_id' => $lastBook['id']]);
+    $lastPage = $req->fetch(PDO::FETCH_ASSOC)['lastPage'] ?? 0;
+}
+
 $i = 0;
 
 while ($i < 50) {
-    $lastLocation = null;
-    
-    $req = $bdd->prepare('SELECT location FROM books ORDER BY id DESC LIMIT 1');
-    $req->execute([]);
-    $data = $req->fetch(PDO::FETCH_ASSOC);
-    
-    if ($data) {
-        $lastLocation = $data['location'];
-    }
-    
-    $hexagonNumber = nextHexagonNumber($lastLocation);
-
-    $wall = 1;
-
     while ($wall <= 4) {
-        $shelf = 1;
-
         while ($shelf <= 5) {
-            $volume = 1;
-            
             while ($volume <= 32) {
-                $bookTitle = generateBookTitle($alphabet);
+                if ($lastPage == 0) {
+                    $bookTitle = generateBookTitle($alphabet);
+
+                    $ins = $bdd->prepare('INSERT INTO books (location, wall, shelf, volume, title) VALUES (:location, :wall, :shelf, :volume, :title)');
+                    $ins->execute([
+                        'location' => $hexagonNumber,
+                        'wall' => $wall,
+                        'shelf' => $shelf,
+                        'volume' => $volume,
+                        'title' => $bookTitle
+                    ]);
+    
+                    $req = $bdd->prepare('SELECT id FROM books ORDER BY id DESC LIMIT 1');
+                    $req->execute([]);
+                    $data = $req->fetch(PDO::FETCH_ASSOC);
+                } else {
+                    $data = $lastBook;
+                }
                 
-                $ins = $bdd->prepare('INSERT INTO books (location, wall, shelf, volume, title) VALUES (:location, :wall, :shelf, :volume, :title)');
-                $ins->execute([
-                    'location' => $hexagonNumber,
-                    'wall' => $wall,
-                    'shelf' => $shelf,
-                    'volume' => $volume,
-                    'title' => $bookTitle
-                ]);
-
-                $req = $bdd->prepare('SELECT id FROM books ORDER BY id DESC LIMIT 1');
-                $req->execute([]);
-                $data = $req->fetch(PDO::FETCH_ASSOC);
-
-                $numberPages = 1;
+                $bookId = $data['id'];
+                $numberPages = $lastPage + 1;
+                $lastPage = 0;
 
                 while ($numberPages <= 410) {
                     $page = generatePage($alphabet);
@@ -69,11 +78,18 @@ while ($i < 50) {
                 $volume++;
             }
             
+            $volume = 1;
             $shelf++;
         }
         
+        $shelf = 1;
         $wall++;
     }
 
+    $hexagonNumber = nextHexagonNumber($hexagonNumber);
+    $wall = 1;
+    $shelf = 1;
+    $volume = 1;
+    
     $i++;
 }
